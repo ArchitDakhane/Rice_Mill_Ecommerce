95b3e194bed0cba1d772a052676e0ee5a025e4c4
Archit Dakhane <architcdakhane24@gmail.com>
2024-12-21 23:24:46 +0530

    Resolve the bug in navbar

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,3 +1,17 @@
+// Navbar
+
+function navbar() {
+    var x = document.getElementById("myTopnav")
+    if (x.className == "navbar") {
+        x.className += " responsive"
+    }
+    else {
+        x.className = "navbar"
+    }
+}
+
+
+// Slideshows
 window.onload = function() {
     let slideIndex = 0
         showSlides()
